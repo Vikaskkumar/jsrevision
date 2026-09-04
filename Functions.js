87c32e9cdd4f1cf6ece337788bc3b49d10d65d80
Vikaskkumar <kvikaskumar040@gmail.com>
2026-09-04 14:17:55 +0530

    functions complete

--- Functions.js
+++ Functions.js
@@ -92,3 +92,151 @@
 // filter()
 // reduce()
 // forEach()
+
+
+//foreach()--->  it not creates a new array
+
+// let arr = [1,3,5,7];
+// arr.forEach(num=>{
+//     console.log(num);
+// })
+
+
+//map()---> mostly used in react. _----> it creates a new array.
+// example-->
+// let numbers = [2,4,6];
+
+// let squares = numbers.map(num =>{
+//     return num*num;
+// })
+
+// console.log(squares)
+
+
+//filter --> gives a new arry with filtration.
+// ex->
+// let numbers = [2,3,4,5,6,7,8];
+// let even = numbers.filter(num=>{
+//     if(num % 2 == 0){
+//         return num
+//     }
+// })
+
+// let odd = numbers.filter(num=>{
+//     if(num % 2 != 0){
+//         return num
+//     }
+// })
+
+// console.log(`even numbers = ${even}`)
+// console.log(`odd numbers = ${odd}`)
+
+
+//reduce()---> it gives a single value
+// const numbers = [1, 2, 3, 4];
+
+// const total = numbers.reduce((sum, number) => {
+//     return sum + number;
+// }, 0);
+
+// console.log(total);
+
+
+
+
+
+
+//rest operator---
+// example-->
+// function sum(...numbers){
+//     return numbers.reduce((total,num)=>{
+//         return total + num;
+//     })
+// }
+
+// console.log(sum(2,3,4,1))
+
+
+
+
+//IIFE---> immediately invoked function expression --> it will automatically called when we run program.
+
+// (function greeting(){
+//     console.log("hello")
+// })();
+
+
+
+
+
+
+
+
+
+//-----3-----advance functions ------
+
+// 3.1 Closures ⭐⭐⭐
+
+// A closure happens when an inner function remembers variables from its outer function even after the outer function has finished executing.
+
+// function outer() {
+//     let count = 0;
+
+//     function inner() {
+//         count++;
+//         console.log(count);
+//     }
+
+//     return inner;
+// }
+
+// const counter = outer();
+
+// counter(); // 1
+// counter(); // 2
+// counter(); // 3
+
+
+// Closures are important for:
+
+// Data privacy
+// Counters
+// Function factories
+// React hooks concepts
+// Callbacks
+// Event handlers
+
+
+//recurstion --> function calls itself.
+// example ---> factorial.-->
+// function factorial(num){
+//     if(num == 0 || num == 1){
+//         return 1
+//     }
+//     else{
+//         return num * factorial(num-1);
+//     }
+// }
+
+// console.log(factorial(4))
+
+
+// 3.5 call()
+// call() lets you call a function with a specific this.----> or simply it allows us to pass a object to a function.
+
+// function greet(){
+//     console.log(this.name);
+// }
+
+// const user = {
+//     name:"vikas"
+// }
+
+// greet.call(user)
+
+
+
+//apply
+//bind
+
+
---
+++ Strings.js
@@ -0,0 +1,22 @@
+//string ---> string is a sequence of characters used to store text.
+// example --> 
+// let name = "vikas"
+// let city = "jaipur"
+
+// we can create stings using three keywords--->  "vikas", 'vikas' , `vikas`
+
+//string indexing---> each character has a index.
+// example--> let name = "vikas"   --   v-0, i-1 , k-2 , a-3 ,s-4----> indexing starts from 0.
+
+
+// str.length---> return length of string   eg--> "vikas".length----> 5   its a property not a method.
+
+
+
+
+
+
+
+//string methods-->
+
+// 
